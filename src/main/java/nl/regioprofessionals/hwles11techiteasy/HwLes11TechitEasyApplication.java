bd5b6cf2d39aed2561c8058cf84be9ba651a9984
RegioProfessionals <richardvanbeem@live.nl>
2025-02-10 14:30:47 +0100

    Repository probleempjes opgelost voor het maken van een version-control van een Spring Boot project.

---
+++ src/main/java/nl/regioprofessionals/hwles11techiteasy/HwLes11TechitEasyApplication.java
@@ -0,0 +1,13 @@
+package nl.regioprofessionals.hwles11techiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwLes11TechitEasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwLes11TechitEasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/regioprofessionals/hwles11techiteasy/HwLes11TechitEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.regioprofessionals.hwles11techiteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwLes11TechitEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
